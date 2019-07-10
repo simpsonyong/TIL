aaeb87999a16c21191f59b3fcf228b647559491f
Seung Yong Shin <simpseungyong@gmail.com>
2019-07-10 16:54:50 +0900

    startcamp | python practice, lotto AIP, flask, html | 190710

--- 00_startcamp/day3/stock.py
+++ 00_startcamp/day3/stock.py
@@ -1,5 +1,7 @@
 from iexfinance.stocks import Stock
+import random
 
-compay = Stock('TSLA', token='pk_34947d6f20564c52a9dcd62bf4d4ab5f')
+companies = ['AAPL', 'GOOGL', 'TSLA', 'FB', 'AMZN']
+company = Stock(random.choice(companies), token='pk_34947d6f20564c52a9dcd62bf4d4ab5f')
 
 print(company.get_quote())
